915f550aadb77999f2b352389a74fd8faac2561e
Ivan Dzyzenko <intale.a@gmail.com>
2026-07-15 16:09:53 +0200

    Adjust subscriptions position migration: cover case when :from_position is present, but subscription was never run

--- db/migrations/20_migrate_subscriptions_current_position.sql
+++ db/migrations/20_migrate_subscriptions_current_position.sql
@@ -4,4 +4,16 @@ set current_position = (select subscription_position
                         where global_position <= subscriptions.current_position
                         order by global_position desc
                         limit 1)
-where current_position is not null
+where current_position is not null;
+
+update subscriptions
+set options = jsonb_set(
+    options,
+    '{from_position}',
+    to_jsonb(coalesce((select subscription_position
+                       from event_subscription_positions
+                       where global_position <= (subscriptions.options ->> 'from_position')::bigint
+                       order by global_position desc
+                       limit 1), 0))
+)
+where options ? 'from_position';
